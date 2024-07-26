50ff476ba983e493b6ddb2167a12c52a1f566516
rohzalbert <roselineogwunte@gmail.com>
2024-07-26 19:53:16 +0100

    first typescript

---
+++ index.ts
@@ -0,0 +1,26 @@
+
+interface Vehicle {
+  make: string;
+  model: string;
+  year: number;
+  start(): void;
+}
+
+class Car implements Vehicle {
+  make: string;
+  model: string;
+  year: number;
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  start(): void {
+    console.log("Car engine started");
+  }
+}
+
+const myCar = new Car("Toyota", "Camry", 2020);
+myCar.start();  
